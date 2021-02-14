cf4f2b4b815139b575f0f278cdaace7d2938a361
Vadym <vadim_hv@ukr.net>
2021-02-14 19:00:47 +0200

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src='https://www.csuohio.edu/sites/default/files/Shine%20Well-logo-social-web.png' />
+        </header>
+    )
+}
+
+export default Header;
